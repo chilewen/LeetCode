d13ebbd462d5fa1a3f921b6df0bd3693ae728388
chilewen <chilewen@126.com>
2021-05-20 20:52:09 +0800

    first commit in 2021.5.20

---
+++ LeetCode/LeetCode/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  LeetCode
+//
+//  Created by wenzl on 2021/5/20.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
